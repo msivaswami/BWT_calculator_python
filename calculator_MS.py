00bcf8c01d17b7e289bee0ee7d942e2a8c53442d
Muthukumaraswami Sivaswami <mxs1179@student.bham.ac.uk>
2023-12-06 18:17:47 +0400

    Written a Python version of the string size calculator.

--- calculator_MS.py
+++ calculator_MS.py
@@ -1,8 +1,7 @@
-import dash
-from dash import dcc, html
-from dash.dependencies import Input, Output
-import sys
+# This is an implementation of the string size calculator in Python, written by M Sivaswami,
+# for the BWT Programming project, as part of the coursework for the NGS module
 
+import sys
 
 # Function to calculate the size of a string in bytes
 def calculate_size(input_string):
@@ -22,49 +21,30 @@ strings_to_process = [
     "ATATGTATACAT"
 ]
 
-# Initialize the Dash app
-app = dash.Dash(__name__)
+# Loop to restart the application
+while True:
+    print("Welcome to a python implementation of M Sivaswami's String Size Calculator!")
+    print("======================================")
 
-# Define the layout of the app
-app.layout = html.Div([
-    html.H1("Calculating the String Size"),
+    # Get user input
+    use_predefined = input("Do you want to use predefined strings? (yes/no): ")
 
-    dcc.Checklist(
-        id='use_predefined',
-        options=[{'label': 'Use predefined strings?', 'value': True}],
-        value=[True]
-    ),
-
-    dcc.Input(id='input_string_custom', type='text', value='', style={'width': '50%'}),
-
-    dcc.Dropdown(
-        id='input_string',
-        options=[{'label': string, 'value': string} for string in strings_to_process],
-        value=strings_to_process[0],
-        style={'width': '50%'}
-    ),
-
-    html.Div(id='output_size')
-])
-
-
-# Define callback to update the output based on user input
-@app.callback(
-    Output('output_size', 'children'),
-    [Input('use_predefined', 'value'),
-     Input('input_string_custom', 'value'),
-     Input('input_string', 'value')]
-)
-def update_output(use_predefined, input_string_custom, input_string):
-    if use_predefined:
-        input_string = input_string
+    if use_predefined.lower() == "yes":
+        print("Choose a predefined string:")
+        for i, string in enumerate(strings_to_process):
+            print(f"{i + 1}. {string}")
+        choice = int(input("Enter the number corresponding to your choice: "))
+        input_string = strings_to_process[choice - 1]
     else:
-        input_string = input_string_custom
+        input_string = input("Enter your custom string: ")
 
+    # Calculate and print the size of the input string
     size_in_bytes = calculate_size(input_string)
-    return f"Size in bytes: {size_in_bytes}"
+    print(f"Size in bytes: {size_in_bytes}")
 
+    # Ask if the user wants to restart the application
+    restart = input("Do you want to calculate the size of another string? (yes/no): ")
+    if restart.lower() != "yes":
+        break
 
-# Run the app
-if __name__ == '__main__':
-    app.run_server(debug=True)
+print("Thank you for using M Sivaswami's String Size Calculator. Goodbye!")
